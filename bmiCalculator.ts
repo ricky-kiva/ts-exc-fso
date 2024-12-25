fa17e8069bdde6e9b17d61745a9edea99ce8f410
ricky-kiva <ricky.kiva@gmail.com>
2024-12-25 13:13:23 +0700

    feat(src): add BMI Calculator func.

---
+++ bmiCalculator.ts
@@ -0,0 +1,27 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const heightInMeter = (height / 100)
+  const bmi = weight / (heightInMeter * heightInMeter);
+
+  switch(true) {
+    case bmi < 16:
+      return 'Underweight (Severe thinness)';
+    case bmi >= 16 && bmi <= 16.9:
+      return 'Underweight (Moderate thinness)';
+    case bmi >= 17.0 && bmi <= 18.4:
+      return 'Underweight (Mild thinness)';
+    case bmi >= 18.5 && bmi <= 24.9:
+      return 'Normal range';
+    case bmi >= 25.0 && bmi <= 29.9:
+      return 'Overweight (Pre-obese)';
+    case bmi >= 30.0 && bmi <= 34.9:
+      return 'Obese (Class I)';
+    case bmi >= 35.0 && bmi <= 39.9:
+      return 'Obese (Class II)';
+    case bmi >= 40.0:
+      return 'Obese (Class III)';
+    default:
+      throw new Error('an error occured while calculating the BMI');
+  }
+}
+
+console.log(calculateBmi(180, 74));
