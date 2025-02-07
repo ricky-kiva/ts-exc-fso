27d1cae366bc4f807a69a94eb7aa18a59df23cc9
ricky-kiva <ricky.kiva@gmail.com>
2025-02-07 14:21:17 +0700

    feat(index): add `/bmi` endpoint

--- index.ts
+++ index.ts
@@ -1,10 +1,30 @@
 import express from 'express';
+import calculateBmi from './bmiCalculator';
 
 const app = express();
 const PORT = 3003;
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+  try {
+    const height = Number(req.query.height);
+    const weight = Number(req.query.weight);
+
+    if (isNaN(height) || isNaN(weight)) {
+      res.status(400).send({ error: "malformatted parameters" });
+      return;
+    }
+
+    const bmi = calculateBmi(height, weight);
+
+    res.send({ weight, height, bmi })
+  } catch (e: unknown) {
+    res.status(400).send('Sorry, there\'s an error calculating the BMI!');
+    if (e instanceof Error) console.log(e);
+  }
 });
 
 app.listen(PORT, () => {
